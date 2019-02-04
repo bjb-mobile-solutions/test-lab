ad6035edf738ea08ea978f271fa8c1223848a904
Development <development@MacBook-Pro.local>
2019-02-04 10:55:23 +0100

    Updates

---
+++ precache-manifest.d7bea8c58a9c874cb57fa89e06afdfb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd521991c94d366e53b5",
+    "url": "/test-lab/static/js/runtime~main.bd521991.js"
+  },
+  {
+    "revision": "e0e6abc25762988ab2bc",
+    "url": "/test-lab/static/js/main.e0e6abc2.chunk.js"
+  },
+  {
+    "revision": "60fe7f164181510bb0f5",
+    "url": "/test-lab/static/js/1.60fe7f16.chunk.js"
+  },
+  {
+    "revision": "e0e6abc25762988ab2bc",
+    "url": "/test-lab/static/css/main.372a073c.chunk.css"
+  },
+  {
+    "revision": "90a5afff7a6a539d7a475702e0ec2cbf",
+    "url": "/test-lab/index.html"
+  }
+];
